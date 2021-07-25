cd38c203ce8658aac5a3bdeaf23c806660f9d588
Sebastian Pape <s@pape.io>
2021-07-25 16:27:54 +0300

    bye prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,7 @@ module.exports = {
   },
   settings: {
   },
-  extends: [
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
+  extends: [],
   plugins: [],
   root: true,
   env: {
--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: false,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 100,
-  tabWidth: 2,
-  bracketSpacing: true,
-  endOfLine: 'auto'
-}
