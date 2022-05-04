52cb0d4e1398fdc1a386b79b99e1dd9843ba77e9
Sebastian Pape <s@pape.io>
2022-05-04 13:58:59 +0200

    v4.5.0: add major tokens to polygon + rebrand BNB smart chain (former binance smart chain)

--- tests/units/findByName.spec.js
+++ tests/units/findByName.spec.js
@@ -4,7 +4,7 @@ describe('findByName', () => {
   
   it('provides a blockchain by name', () => {
     expect(Blockchain.findByName('ethereum').label).toEqual('Ethereum')
-    expect(Blockchain.findByName('bsc').label).toEqual('Binance Smart Chain')
+    expect(Blockchain.findByName('bsc').label).toEqual('BNB Smart Chain')
     expect(Blockchain.findByName('polygon').label).toEqual('Polygon')
   });
 });
--- tests/units/findByNetworkId.spec.js
+++ tests/units/findByNetworkId.spec.js
@@ -4,7 +4,7 @@ describe('findByNetworkId', () => {
   
   it('provides a blockchain by name', () => {
     expect(Blockchain.findByNetworkId(1).label).toEqual('Ethereum')
-    expect(Blockchain.findByNetworkId('56').label).toEqual('Binance Smart Chain')
+    expect(Blockchain.findByNetworkId('56').label).toEqual('BNB Smart Chain')
     expect(Blockchain.findByNetworkId(137).label).toEqual('Polygon')
   });
 });
